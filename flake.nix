30b62a4dbcf680902f9b1cebd4298717a1941052
brokenpike <brokenpike@garmr.org>
2024-08-22 21:00:29 +0200

    configured homemanager

--- flake.nix
+++ flake.nix
@@ -5,9 +5,17 @@
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+        home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
+  outputs = { self, nixpkgs, nixos-hardware,home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -16,6 +24,16 @@
         # so the old configuration file still takes effect
         ./configuration.nix
         nixos-hardware.nixosModules.framework-13-7040-amd
+        home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # TODO replace ryan with your own username
+            home-manager.users.scott = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
       ];
     };
   };
---
+++ home.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  # TODO please change the username & home directory to your own
+  home.username = "scott";
+  home.homeDirectory = "/home/scott";
+
+  home.packages = with pkgs; [
+  cowsay
+  ];
+    programs.git = {
+    enable = true;
+    userName = "brokenpike";
+    userEmail = "brokenpike@garmr.org";
+  };
+  home.stateVersion = "24.05";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
